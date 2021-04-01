38ba8cc90fbb9911bff8f992f1fbff02e9fd31f2
yuyaya777 <yuyaya777@gmail.com>
2021-04-01 05:41:10 +0000

    Commit2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,4 @@
 class BooksController < ApplicationController
-  def top
-  end
 
   def index
     @books = Book.all
@@ -8,11 +6,13 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
@@ -27,7 +27,7 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
+    if @book.update
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_09_15_014227) do
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
